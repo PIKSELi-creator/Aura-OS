33fbf54d350461778db3ca481e4bf9815f731594
PIKSELi-creator <aminosav898@gmail.com>
2026-07-29 03:27:43 +0300

    Add AppInfo data class for application details

---
+++ app/src/main/java/com/auraos/system/AppInfo.kt
@@ -0,0 +1,9 @@
+package com.auraos.system; // Замени на имя твоего пакета
+
+import android.graphics.drawable.Drawable
+
+data class AppInfo(
+    val label: String,
+    val packageName: String,
+    val icon: Drawable
+)
