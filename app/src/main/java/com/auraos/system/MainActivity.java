acf21d8aa38d8af828144c6588ec6d60cf5273ed
PIKSELi-creator <aminosav898@gmail.com>
2026-07-29 04:13:46 +0300

    Implement search feature in MainActivity
    Added search functionality to filter installed apps.

--- app/src/main/java/com/auraos/system/MainActivity.java
+++ app/src/main/java/com/auraos/system/MainActivity.java
@@ -2,30 +2,51 @@ package com.auraos.system;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.GridView;
 import java.util.List;
 
 public class MainActivity extends Activity {
+
+    private AppAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 1. Получаем список всех установленных приложений
+        // 1. Получаем список приложений
         List<AppInfo> apps = AppManager.INSTANCE.getInstalledApps(this);
 
-        // 2. Находим сетку на экране
+        // 2. Настраиваем сетку
         GridView gridView = findViewById(R.id.apps_grid);
-
-        // 3. Передаем список в наш адаптер и выводим на экран
-        AppAdapter adapter = new AppAdapter(this, apps);
+        adapter = new AppAdapter(this, apps);
         gridView.setAdapter(adapter);
+
+        // 3. Настраиваем реальный поиск
+        EditText searchBox = findViewById(R.id.search_box);
+        searchBox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (adapter != null) {
+                    adapter.filter(s.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
     }
 
     @Override
     public void onBackPressed() {
-        // Блокируем кнопку "Назад", чтобы пользователь не мог "выйти" из лончера
+        // Блокируем кнопку "Назад"
     }
 }
+
 
