b3640a317e9a902e9bdcabcc69e0a0d4f390c6ab
PIKSELi-creator <aminosav898@gmail.com>
2026-07-29 03:48:52 +0300

    Log number of installed applications on startup
    Added logging for the number of installed applications.

--- app/src/main/java/com/auraos/system/MainActivity.java
+++ app/src/main/java/com/auraos/system/MainActivity.java
@@ -2,6 +2,8 @@ package com.auraos.system;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -9,10 +11,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Загружаем список приложений и выводим количество в лог
+        List<AppInfo> apps = AppManager.INSTANCE.getInstalledApps(this);
+        Log.d("AuraOS", "Найдено приложений: " + apps.size());
     }
 
     @Override
     public void onBackPressed() {
-        // Блокируем выход с рабочего стола Aura OS
+        // Блокируем выход с рабочего стола
     }
 }
+
