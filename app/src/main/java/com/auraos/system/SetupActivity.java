7bd67f3a7bc7803ec3acf04249f157d9728e429e
PIKSELi-creator <aminosav898@gmail.com>
2026-07-29 13:47:48 +0300

    Convert SetupActivity from Kotlin to Java

---
+++ app/src/main/java/com/auraos/system/SetupActivity.java
@@ -0,0 +1,34 @@
+package com.auraos.system;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+
+        Button btnStart = findViewById(R.id.btn_start);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPref = getSharedPreferences("AuraSettings", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putBoolean("isFirstRun", false);
+                editor.apply();
+
+                Intent intent = new Intent(SetupActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
